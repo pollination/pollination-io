a45ff45159054c2fdb71d81e7c991d685c06a148
Mikkel Pedersen <mkklpdrsn@gmail.com>
2026-02-26 12:26:15 +0100

    fix(deploy): Update deployment using build

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ fi
 
 echo "PyPi Deployment..."
 echo "Building distribution"
-python setup.py sdist bdist_wheel
+python -m build
 echo "Pushing new version to PyPi"
-twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD 
+twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
